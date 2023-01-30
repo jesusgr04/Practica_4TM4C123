8ef9e7e85d616c01391c858ed193be72f5cba16a
JesusHumberto <jgarcia265@alumnos.uaq.mx>
2023-01-30 00:41:31 -0600

    1.1

--- ADC.c
+++ ADC.c
@@ -1,7 +1,7 @@
 #include "lib/include.h"
 extern void Configura_Reg_ADC(void)
 {
-/*
+/* jhgr
     b) 2,4,6,7,10,1- 30MHZ 57600 -sec3, sec1
                                                     PIN - CANAL
                                                     PE2 - AIN1,
@@ -20,32 +20,37 @@ extern void Configura_Reg_ADC(void)
     //                     F     E      D       C      B     A     
     SYSCTL->RCGCGPIO |= (1<<5)|(1<<4)|(1<<3)|(0<<2)|(0<<1)|(1<<0); //Port A -> UART, Port F -> leds, Port E y D para canales 
     //Pag 663 (GPIODIR) Habilta los pines como I/O un cero para entrada y un uno para salida. Pag 801 para pines correspondientes a canales
-    GPIOB_AHB->DIR = (0<<4) ;// PIN B4
-    GPIOE_AHB->DIR = (0<<1) | (0<<2); //PE1 PE2
-    GPIOD_AHB->DIR = (0<<3) | (0<<1) | (0<<0); //PD3, PD1 y PD0
+
+    GPIOE->DIR &= ~(1<<2) ;// PIN E2 
+    GPIOE->DIR &= ~(1<<1) ; // PIN E1
+    GPIOB->DIR &= ~(1<<4) ; //PIN B4
+    GPIOD->DIR &= ~(1<<3) ;// PIN D3
+    GPIOD->DIR &= ~(1<<1) ;// PIN D1
+    GPIOD->DIR &= ~(1<<0) ;// PIN D0
+
     //(GPIOAFSEL) pag.672  trabajan con función alternativa 
-    GPIOB_AHB->AFSEL= (0<<4) ;// PIN B4
-    GPIOE_AHB->AFSEL = (0<<1) | (0<<2); //PE1 PE2
-    GPIOD_AHB->AFSEL = (0<<3) | (0<<1) | (0<<0); //PD3, PD1 y PD0
+    GPIOB->AFSEL= (1<<4) ;// PIN B4
+    GPIOE->AFSEL = (1<<1) | (1<<2); //PE1 PE2
+    GPIOD->AFSEL = (1<<3) | (1<<1) | (1<<0); //PD3, PD1 y PD0
     //(GPIODEN) pag.683 desabilita el modo digital de los pines porque son analogicos
-    GPIOB_AHB->DEN = (0<<4) ;// PIN B4
-    GPIOE_AHB->DEN = (0<<1) | (0<<2); //PE1 PE2
-    GPIOD_AHB->DEN = (0<<3) | (0<<1) | (0<<0); //PD3, PD1 y PD0
+    GPIOB->DEN = (0<<4) ;// PIN B4
+    GPIOE->DEN = (0<<1) | (0<<2); //PE1 PE2
+    GPIOD->DEN = (0<<3) | (0<<1) | (0<<0); //PD3, PD1 y PD0
     //Pag 688 GPIOPCTL registro combinado con el GPIOAFSEL y la tabla pag 1351, Se indica la función alternativa de los pines
-    GPIOB_AHB->PCTL = GPIOB_AHB->PCTL & (0xFF0FFFFF); // PONER EN 0 PIN 5 
-    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFFFFF00F); //Poner 0 en los bits de los pines 1, 2 
-    GPIOD_AHB->PCTL = GPIOD_AHB->PCTL & (0xFFFF0F00); //Poner 0 en los bits de los pines 0, 1,3
+    GPIOB->PCTL = GPIOB->PCTL & (0xFF0FFFFF); // PONER EN 0 PIN 5 
+    GPIOE->PCTL = GPIOE->PCTL & (0xFFFFF00F); //Poner 0 en los bits de los pines 1, 2 
+    GPIOD->PCTL = GPIOD->PCTL & (0xFFFF0F00); //Poner 0 en los bits de los pines 0, 1,3
     //(GPIOAMSEL) pag.687 habilitar analogico los pines de los canales
-    GPIOB_AHB->AMSEL = (0<<4) ;// PIN B4
-    GPIOE_AHB->AMSEL = (0<<1) | (0<<2); //PE1 PE2
-    GPIOD_AHB->AMSEL = (0<<3) | (0<<1) | (0<<0); //PD3, PD1 y PD0
+    GPIOB->AMSEL = (1<<4) ;// PIN B4
+    GPIOE->AMSEL = (1<<2) | (1<<1); //PE1 PE2
+    GPIOD->AMSEL = (1<<3) | (1<<1) | (1<<0); //PD3, PD1 y PD0
     //Pag 891 El registro (ADCPC) establece la velocidad de conversión por segundo
     //1Mms  SE pone 7 en binario porque trabaja con la max. velocidad que es 1 millon
-    ADC0->PC = (0<<3)|(1<<2)|(1<<1)|(1<<0);
-    ADC1->PC = (0<<3)|(1<<2)|(1<<1)|(1<<0);
+    ADC0->PC = (1<<2)|(1<<1)|(1<<0);
+    ADC1->PC = (1<<2)|(1<<1)|(1<<0);
     //Pag 841 configurar la prioridad de los secuenciadores 
-    ADC0->SSPRI = 0x1203; //
-    ADC1->SSPRI = 0x0213; //
+    ADC0->SSPRI = 0x3102; //
+    ADC1->SSPRI = 0x1023; //
     //Pag 821 control de la activación de los secuenciadores se desactivan los 4 secuenciadores 
     ADC0->ACTSS  =   (0<<3) | (0<<2) | (0<<1) | (0<<0); 
     ADC1->ACTSS  =   (0<<3) | (0<<2) | (0<<1) | (0<<0);
@@ -55,23 +60,56 @@ extern void Configura_Reg_ADC(void)
     ADC1->EMUX  = (0x0000); 
     //Pag 867 se definen entradas analógicas con el canal y secuenciador seleccionado,
     // Se establece que canal estará en cada mux por lo tanto en cada secuenciador y en cada modulo
-    ADC0->SSMUX1 = 0x0124; // Los canales 1,2, 4 se definen para el mux 1 del secuenciador 1 en el modulo 0
-    ADC1->SSMUX2 = 0x067A; // Los canales 6,7 y 10  se definen para el mux 2 del secuecniador 3 en el modulo 1
+    ADC0->SSMUX1 = 0x0246; // Los canales 1,2, 4 se definen para el mux 1 del secuenciador 1 en el modulo 0
+    ADC1->SSMUX2 = 0x00A1; // Los canales 6,7 y 10  se definen para el mux 2 del secuecniador 3 en el modulo 1
+    ADC1->SSMUX3 = 0x0007;
     //pag 868 Se configura el bit de control de muestreo y la interrupción
     // secuenciador 1        mux 0            mux 1            mux 2      es donde se establecen los canales en la configuración del SSMUX1 -> linea 70 
-    ADC0->SSCTL1 = (1<<2) | (1<<1)| (1<<6) | (1<<5) | (1<<10) | (1<<9); // Para establecer configuración de muestro y finalizar muestro acorde a la pag. 868
+    ADC0->SSCTL1 = 0X00000644;// Para establecer configuración de muestro y finalizar muestro acorde a la pag. 868
     // secuenciador 3        mux 0            mux 1            mux 2  
-    ADC1->SSCTL3 = (1<<2) | (1<<1)| (1<<6) | (1<<5) | (1<<10) | (1<<9);
+    ADC1->SSCTL2 = 0X00000064;
+    ADC1->SSCTL3 = 0X00000006;
     // Pag 825 Enable ADC Interrupt - Interrumpe la conversión del secuenciador a usar 
-    /* Unmask ADC0 sequence 2 interrupt*/ 
     // Al secuenciador 1 y 3  se le asigna un 1 para indicar que manda la señal pura al controlador de interrupciones
     ADC0->IM |= (1<<1); 
-    ADC1->IM |= (1<<3); 
+    ADC1->IM |= (1<<2);
+    ADC1->IM |= (1<<3);
     //Pag 821 (ADCACTSS) Este registro controla la activación de los secuenciadores utilizados 
     // se habilita el secuanciador 1 y 3
     ADC0->ACTSS = (0<<3) | (0<<2) | (1<<1) | (0<<0);
-    ADC1->ACTSS = (1<<3) | (0<<2) | (0<<1) | (0<<0); 
+    ADC1->ACTSS = (1<<3) | (1<<2) | (0<<1) | (0<<0); 
     //se inicializa el secuenciador 1 y 3   cuando se configura el adc en modo de procesador
-    ADC0->PSSI |= (1<<1); 
-    ADC1->PSSI |= (1<<3); 
+    ADC0->PSSI |= (0<<1); 
+    ADC1->PSSI |= (0<<2)|(0<<3); 
+
+    ADC0->ISC = 2; 
+    ADC1->ISC = 12;
 }
+//adquisición 
+extern void ADC0_InSeq1(uint16_t Result[6])
+{
+       ADC0->PSSI = 0x00000002;
+       while((ADC0->RIS&0x02)==0){}; // espera al convertidor
+       Result[0] = ADC0->SSFIFO1&0xFFF; // leer  el resultado almacenado en la pila
+       Result[1] = ADC0->SSFIFO1&0xFFF;
+       Result[2] = ADC0->SSFIFO1&0xFFF;
+       ADC0->ISC = 0x0002; 
+}
+
+extern void ADC1_InSeq2(uint16_t Result[6])
+{
+       ADC1->PSSI = 0x00000004;
+       while((ADC1->RIS&0x04)==0){}; // esperamos  el  convertidor
+       Result[3] = ADC1->SSFIFO2&0xFFF; 
+       Result[4] = ADC1->SSFIFO2&0xFFF; //en FIFO esta el valor de conversión
+       ADC1->ISC = 0x0004;  //conversion finalizada, limpiamos la conversión
+}
+
+extern void ADC1_InSeq3(uint16_t Result[6])
+{
+       ADC1->PSSI = 0x00000008;  //se habilita el modo de configuración y luego espera al convertidor
+       while((ADC1->RIS&0x08)==0){}; 
+       Result[5] = ADC1->SSFIFO3&0xFFF; 
+       ADC1->ISC = 0x0008;
+}
+
--- main.c
+++ main.c
@@ -1,20 +1,56 @@
 #include "lib/include.h"
+
+extern void integer_to_char(uint16_t number) 
+{
+    char number_in_string[5];
+    int a = 3;
+    int k = 0; 
+    int n_digit;  
+    while (a >= 0){
+        n_digit = (int)number / (int)(pow(10, a));
+        number_in_string[k] = n_digit + '0';
+        number = number - (n_digit * pow(10, a)); 
+        a = a - 1; 
+        k++;
+    }
+    number_in_string[4] = '\n';
+    printString(number_in_string);
+}
+
 int main(void)
 {
-    char valor1[4]; //Para enviar los valores del ADC en carácteres, es de 4 porque son cuatro digitos de 4096 
-    char valor2[4];
-    char valor3[4];
-    char valor4[4];
-    char valor5[4];
-    char valor6[4];
+    uint16_t Result[6]; //variables
+    uint16_t valor1; //es tipo char porque se envian por el UART
+    uint16_t valor2;
+    uint16_t valor3;
+    uint16_t valor4;
+    uint16_t valor5;
+    uint16_t valor6;
 
-    Configurar_PLL(_20MHZ);  // velocidad de reloj
+   Configurar_PLL(_20MHZ);  // velocidad de reloj
     Configura_Reg_ADC();
     Configurar_UART0();
-    Configurar_GPIO();
+    // Configurar_GPIO();
 
     while(1)
     {   
+        ADC0_InSeq1(Result); 
+        ADC1_InSeq2(Result);
+        ADC1_InSeq3(Result);
+        
+        valor1 = (uint16_t)(Result[0]);
+        valor2 = (uint16_t)(Result[1]);
+        valor3 = (uint16_t)(Result[2]);
+        valor4 = (uint16_t)(Result[3]);
+        valor5 = (uint16_t)(Result[4]);
+        valor6 = (uint16_t)(Result[5]);
+
+        integer_to_char(valor1); 
+        integer_to_char(valor2); 
+        integer_to_char(valor3); 
+        integer_to_char(valor4);
+        integer_to_char(valor5);
+        integer_to_char(valor6);
     }
 }
 
